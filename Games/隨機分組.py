bc688b2e69544a926bbd2e18674209f567f61934
PGpenguin72 <tu28291797@gmail.com>
2024-08-25 00:49:35 +0800

    新增隨機分組.py

---
+++ Games/隨機分組.py
@@ -0,0 +1,139 @@
+import os
+import random
+import msvcrt
+import time
+from random import choice
+from os import system as cmd
+
+cmd("title 隨機分組小程序")
+cmd("cls")
+print("隨機分組小程式，Made By.PGpenguin72 \n ver. 1.0.0 請勿隨意修改程序碼 \n\n\n")
+print("(3秒後即將開始分組！)")
+time.sleep(3)
+cmd("cls")
+
+while True:
+    input_players = ""
+    while not input_players.strip():
+        input_players = input("請輸入所有成員名單，並以「, 」隔開每個成員的名字(或輸入stop來結束分組)。\n(例如:小明, 小紅, 小黃) \n > > >")
+    input_players = input_players.split(', ')
+
+    input_team = ""
+    while not input_team.strip():
+        input_team = input("請輸入要分的組別名稱,並以「, 」隔開(最多26個組)。\n (企鵝隊, 螃蟹隊, 北極熊隊) \n > > > ")
+    input_team = input_team.split(", ")
+
+    team_Raw = input_team
+    player_raw = input_players
+
+    while True:
+
+        input_team = team_Raw
+        input_players = player_raw
+
+        teams = {}
+
+        fountion = 1 
+
+        for t in input_team:
+            key = f'team_{fountion}'
+            teams[key] = t
+            fountion += 1
+
+        T_fountion = fountion -1 #隊伍總數
+
+        players = {}
+        fountion = 1
+
+        for p in input_players:
+            player = f'player_{fountion}'
+            players[player] = p
+            fountion += 1
+
+        P_list = list(players.values())
+        T_list = [[] for _ in range(T_fountion)]
+
+        fountion -= 1
+        P_fountion = fountion
+        fountion = 1
+
+        while len(P_list) > 0:
+            for i in range(T_fountion):
+                if P_list:
+                    player = choice(P_list)
+                    T_list[i].append(player)
+                    P_list.remove(player)
+
+        print()
+        cmd("cls")
+        for index, team_key in enumerate(teams.keys()):
+            if index < len(T_list):
+                print(f'{teams[team_key]} 成員: {", ".join(T_list[index])}')
+                time.sleep(0.3)
+
+        while True:
+            ans = input("請問是否重新隨機分組?(Y/N) \n > > >")
+            if ans == "Y":
+                print("正在準備重新分組中")
+                cmd("cls")
+                break
+            elif ans == "N":
+                print()
+                while True:
+                    ans_2 = input("那是否要重新開始(R)或統整數據(C) \n (註: 重新開始會刪除所有數據，統整數據會只保留結果於終端機。) \n > > > ")
+                    if ans_2 == "R":
+                        print()
+                        break
+                    elif ans_2 == "C":
+                        print()
+                        break
+                    else:
+                        print("請確認字母大小寫或是否有輸入錯誤")
+                        continue
+                if ans_2 == "R":
+                    print()
+                    break
+                elif ans_2 == "C":
+                    print()
+                    break
+            elif ans == "":
+                continue
+            else:
+                print("請確認字母大小寫或是否有輸入錯誤")
+                continue                       
+
+            if ans_2 == "R":
+                print("正在清除檔案")        
+                time.sleep(3)
+                break  
+            elif ans_2 == "C":
+                print("正在記錄數據")
+                time.sleep(2)
+                break 
+        
+        if ans == "Y":
+            continue
+        elif ans_2 == "R":
+            cmd("cls")
+            break
+        elif ans_2 == "C":
+            print()
+            break
+    if ans_2 == "R":
+        cmd("cls")
+        continue
+    elif ans_2 == "C":
+        print()
+        break                 
+
+print()
+cmd("cls")
+for index, team_key in enumerate(teams.keys()):
+    if index < len(T_list):
+        print(f'{teams[team_key]} 成員: {", ".join(T_list[index])}')
+        time.sleep(0.2)
+
+print("\n\n")
+print("隨機分組小程式，Made By.PGpenguin72 \n ver. 1.0.0 請勿隨意修改程序碼")
+print("\n\n\n按下任意鍵離開或關閉程序")
+msvcrt.getch()
--- main.py
+++ main.py
@@ -6,23 +6,39 @@ from os import system as cmd
 location = os.path.dirname(__file__)
 Games = os.path.join(location, "Games")
 
-cmd("cls")
-print("小遊戲大集結，Made By.PGpenguin72 \n ver. 1.0.4 請勿隨意修改程序碼")
-print("目前擁有的程序:")
+while True:
+    cmd("cls")
+    print("小遊戲大集結，Made By.PGpenguin72 \n ver. 1.0.5 請勿隨意修改程序碼")
+    print("目前擁有的程序:")
 
-for dirpath, dirnames, filenames in os.walk(Games):
-    for filename in filenames:
-        name, ext = os.path.splitext(filename)
-        print(">", name)
-    
-Uinput = input("請問你想玩什麼遊戲?")
-if Uinput == "猜拳":
-    os.chdir(Games)
-    cmd("cls")
-    cmd("python 猜拳.py")
-elif Uinput == "賽龜":
-    os.chdir(Games)
-    cmd("cls")
-    cmd("python 賽龜.py")
-else:
-    print("目前不支持此軟件，請重新啟動main.py")
+    for dirpath, dirnames, filenames in os.walk(Games):
+        for filename in filenames:
+            name, ext = os.path.splitext(filename)
+            print(">", name)
+
+    Uinput = input("請問你想玩什麼遊戲? \n > > > ")
+    if Uinput == "猜拳":
+        os.chdir(Games)
+        print("\n\n\n 正在啟動小程序:D") 
+        time.sleep(3)
+        cmd("cls")
+        cmd("python 猜拳.py")
+        break
+    elif Uinput == "賽龜":
+        os.chdir(Games)
+        print("\n\n\n 正在啟動小程序:D")
+        time.sleep(3)
+        cmd("cls")
+        cmd("python 賽龜.py")
+        break
+    elif Uinput == "隨機分組":
+        os.chdir(Games)
+        print("\n\n\n 正在啟動小程序:D")
+        time.sleep(3)
+        cmd("cls")
+        cmd("python 隨機分組.py")
+        break
+    else:
+        print("\n\n\n (目前不支持此軟件，請重新輸入)")
+        time.sleep(3)
+        continue
