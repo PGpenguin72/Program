9ab8649a3e770de32317a566db3cc59190c4b715
PGpenguin72 <tu28291797@gmail.com>
2024-08-28 16:50:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ ver = {
 
 while True:
     cmd("cls")
-    print("小遊戲大集結，Made By.PGpenguin72 \n ver. 1.0.5 請勿隨意修改程序碼")
+    print("小遊戲大集結，Made By.PGpenguin72 \n ver. 1.0.6 請勿隨意修改程序碼")
     print("目前擁有的程序:")
 
     for dirpath, dirnames, filenames in os.walk(Games):
